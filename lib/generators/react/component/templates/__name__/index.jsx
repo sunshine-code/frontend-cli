70ca59ec00458b025e4482eb954ed9ba85a66264
Myroslav Kiurchev <kiurchv@gmail.com>
2016-07-14 00:49:45 +0300

    WIP: Add eslint support

--- lib/generators/react/component/templates/__name__/index.jsx
+++ lib/generators/react/component/templates/__name__/index.jsx
@@ -1,11 +1,18 @@
-import React, { Component } from "react";
+import React, { Component<% if (children) { %>, PropTypes<% } %> } from "react";
 <% if (styles) { %>
 import style from "./style.<%= stylesExt %>";
 <% } %>
 export default class <%= name %> extends Component {
+  static propTypes = {<% if (children) { %>
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node
+    ]).isRequired
+  <% } %>}
+
   render() {
     return (
-      <div><% if (content) { %>
+      <div<% if (styles) { %> className={style.root}<% } %>><% if (content) { %>
         <%= content %><% } %><% if (children) { %>
         {this.props.children}<% } %>
       </div>
